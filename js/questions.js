4211b730e64189e0844cb08106b1e1a244dd3362
venrable18 <onenvictor@gmail.com>
2022-09-06 11:23:37 -0400

    first commit

---
+++ js/questions.js
@@ -0,0 +1,105 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+ 
+    {
+    numb: 6,
+    question: "The  following are the various types of CSS",
+    answer: "CSS3",
+    options: [
+      "CSS1",
+      "CSS2",
+      "CSS3",
+      "CSS4",
+    ]
+  },
+    {
+    numb: 6,
+    question: "What are the advantages of CSS",
+    answer: "BandWidth",
+    options: [
+      "BandWidth",
+      "Site-width consistency",
+      "Accessibility",
+      "presentation"
+    ]
+  },
+    {
+    numb: 7,
+    question: "Most used CSS frameworks is?",
+    answer: "Bootstrap",
+    options: [
+      "Bootstrap",
+      "Material UI",
+      "Tailwland CSS",
+      "None of the Above"
+    ]
+  },
+
+
+    {
+    numb: 8,
+    question: "some CSS components are?",
+    answer: "All of the above",
+    options: [
+      "Selector",
+      "Property",
+      "Value",
+      "All of the above"
+    ]
+  },
+];
